a0594042045b465c8558656538639c1e1843ae68
mfrancia <matteo.francia2@studio.unibo.it>
2021-10-25 12:18:03 +0200

    working on CI

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ then
   export $(cat .env | sed 's/#.*//g' | xargs)
 fi
 docker-compose up --build -d
-./wait-for-it.sh 127.0.0.1:${MONGO_PORT} --strict --timeout=10 -- echo "Mongo is up"
+./wait-for-it.sh ${MONGO_URL}:${MONGO_PORT} --strict --timeout=10 -- echo "Mongo is up"
 npm run test
 # docker build . -t nosql/nodejs
 # docker run -p 8080:8080 -d nosql/nodejs
