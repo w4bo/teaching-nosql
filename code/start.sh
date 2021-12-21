d733553ae69bfdb75e4f8a581aa4e92525b2b09c
mfrancia <matteo.francia2@studio.unibo.it>
2021-12-21 09:06:09 +0100

    fix pwd

--- code/start.sh
+++ code/start.sh
@@ -8,7 +8,7 @@ fi
 docker-compose down
 docker-compose up --build -d
 ./wait-for-it.sh ${MONGO_URL}:${MONGO_PORT} --strict --timeout=10 -- echo "Mongo is up"
-docker exec graphdb bash -c "neo4j-admin set-initial-password ${NEO4J_PWD}"
+# docker exec graphdb bash -c "neo4j-admin set-initial-password ${NEO4J_PWD}"
 ./wait-for-it.sh ${NEO4J_URL}:7474 --strict --timeout=10 -- echo "Neo4J 7474 is up"
 ./wait-for-it.sh ${NEO4J_URL}:7687 --strict --timeout=10 -- echo "Neo4j 7687 is up"
 sleep 15
