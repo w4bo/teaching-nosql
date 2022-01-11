6ee5528a51e06788114a66206aa43b11edc10ac9
Matteo Francia <m.francia@unibo.it>
2022-01-11 17:39:43 +0100

    Create download.bat

---
+++ code/download.bat
@@ -0,0 +1,10 @@
+cd code
+cd mongodb\data
+curl http://big.csr.unibo.it/projects/nosql-datasets/games.bson
+curl http://big.csr.unibo.it/projects/nosql-datasets/restaurants.bson
+curl http://big.csr.unibo.it/projects/nosql-datasets/yelp-business.bson
+dir
+cd ..\..
+cd mysql\data
+curl http://big.csr.unibo.it/projects/nosql-datasets/foodmart.sql
+cd ..\..
