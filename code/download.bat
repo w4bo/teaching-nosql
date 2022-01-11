cb8a0596e384528c7fa74b1fdd02b17843eee961
Matteo Francia <m.francia@unibo.it>
2022-01-11 17:46:01 +0100

    Update download.bat

--- code/download.bat
+++ code/download.bat
@@ -1,10 +1,10 @@
 cd code
 cd mongodb\data
-curl http://big.csr.unibo.it/projects/nosql-datasets/games.bson
-curl http://big.csr.unibo.it/projects/nosql-datasets/restaurants.bson
-curl http://big.csr.unibo.it/projects/nosql-datasets/yelp-business.bson
+curl -o games.bson http://big.csr.unibo.it/projects/nosql-datasets/games.bson
+curl -o restaurants.bson http://big.csr.unibo.it/projects/nosql-datasets/restaurants.bson
+curl -o yelp-business.bson http://big.csr.unibo.it/projects/nosql-datasets/yelp-business.bson
 dir
 cd ..\..
 cd mysql\data
-curl http://big.csr.unibo.it/projects/nosql-datasets/foodmart.sql
+curl -o foodmart.sql http://big.csr.unibo.it/projects/nosql-datasets/foodmart.sql
 cd ..\..
