351cbbe2d0f557817824dfb550fbd4afbab4bc6c
mfrancia <matteo.francia2@studio.unibo.it>
2022-02-03 10:32:44 +0100

    fix readme

--- code/start.bat
+++ code/start.bat
@@ -2,7 +2,7 @@ REM Cleaning exiting docker containers
 FOR /f "tokens=*" %%i IN ('docker ps -q') DO docker stop %%i
 FOR /f "tokens=*" %%i IN ('docker ps -q') DO docker rm %%i
 docker system prune -f
-FOR /f "tokens=*" %%i IN ('docker volume ls --filter dangling=true -q') DO docker volume rm %%i
+FOR /f "tokens=*" %%i IN ("docker volume ls -qf dangling=true") DO docker volume rm %%i
 
 REM Starting docker containers
 docker-compose down
